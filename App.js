022b03a6ea99ee800dd1220070e79d5adceb6dbc
Stan K <motrhelp@stans-air.home>
2022-03-29 21:50:24 +0200

    Expo app, layout and header

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>GEOGLE</Text>
+      </View>
+      <View style={styles.gameContainer}></View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  headerContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  gameContainer: {
+    flex: 9
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: "bold"
+  }
+
+});
