0c567ea7399e82c06583283f708909b3224f1923
usikninam <usikninam@gmail.com>
2022-04-29 18:05:47 +0200

    Attempt to write tests

---
+++ tests/CountryList.test.js
@@ -0,0 +1,9 @@
+// const countryList = require('../data/CountryList');
+
+// countryList.forEach(country => console.log(element));
+// test("Check flag entity for each country", () => {
+//     countryList.forEach(country => {
+//         console.log(country.name);
+//     });
+//     });
+
